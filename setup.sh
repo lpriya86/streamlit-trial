5ae2e30aba14a558d67f849de4cc6dc22c131972
lpriya86 <109868748+lpriya86@users.noreply.github.com>
2022-07-23 22:31:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
+email=\"21f1006060@student.onlinedegree.iitm.ac.in\"\n\
+" >~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless= true\n\
+enableCORS=false\n\
+port=$PORT\n\
+" >~/.streamlit/config.toml
